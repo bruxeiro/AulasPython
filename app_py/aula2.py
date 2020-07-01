6c5a7887a4e39dffb25d88b4f13bfc4a70671392
Gabriel <gabrielrosabueno@hotmail.com>
2020-07-01 08:24:45 -0300

    Identação

---
+++ app_py/aula2.py
@@ -0,0 +1,27 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multi = a * b
+divi = a/b
+resto = a % b
+
+
+print('Soma: {soma}.'
+      ' \nSubtracao: {sub} '
+      '\nMultiplicação: {multi}. '
+      '\nDivisão: {divi}. '
+      '\nResto: {resto}.'
+      .format(soma=soma,
+              sub=subtracao,
+              divi=divi,
+              multi=multi,
+              resto=resto)  )
+
+
+
+# print('Soma: ' + str(soma))
+# print(subtracao)
+# print(multi)
+# print(divi)
+# print(resto)
